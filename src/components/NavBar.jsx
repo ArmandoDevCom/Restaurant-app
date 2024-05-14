4f13fb17c142aed92bbf2ee8a5b560a4162d9ca6
hugo duran <hadm5@outlook.com>
2024-05-13 23:58:08 -0600

    Nueva nav bar y componentes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,32 @@
 const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid contenedor_navbar">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
-      Bootstrap
+    <nav className="navbar navbar-expand-lg barra_menu">
+  <div className="container-fluid contenedor_barra_menu">
+    <a className="logosake" href>
+      <img src="/img/logo_restaurante_mex.png" alt />
     </a>
-  </div>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#"><font style={{verticalAlign: 'inherit'}}><font style={{verticalAlign: 'inherit'}}>Ordena aquí</font></font></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#"><font style={{verticalAlign: 'inherit'}}><font style={{verticalAlign: 'inherit'}}>Promociones</font></font></a>
         </li>
       </ul>
+      <form className="d-flex" role="search" wtx-context="0E43AC67-7E76-43D6-8851-1414130E106F">
+        <input className="form-control me-2" type="search" placeholder="Buscar platillo" aria-label="Buscar" wtx-context="5EA8204E-BCBD-4AA2-850F-25A1E47D97F8" />
+        <button className="btn btn-warning" type="submit"><font style={{verticalAlign: 'inherit'}}><font style={{verticalAlign: 'inherit'}}>Buscar</font></font></button>
+      </form>
+      <button type="button" className="btn btn-success m-3">Iniciar sesión</button>
     </div>
   </div>
 </nav>
+
+
   )
 }
 
