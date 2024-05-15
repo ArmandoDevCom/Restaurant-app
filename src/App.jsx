f91a5886dab8d453d48fc39bc4d34b6d28128c1f
hugo duran <hadm5@outlook.com>
2024-05-15 14:09:12 -0600

    Iniciando menu y cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header"
 import NavBar from "./components/NavBar"
 import HomePage from "./pages/HomePage"
+import MenuPage from "./pages/MenuPage"
 
 const App = () => {
   return (
+    <>
     <NavBar />
+    <MenuPage />
+    </>
   )
 }
 
